506d6921073f66bef0ce66b6c57f88e10e22a2ce
Conrad Christian <conradchristian812@gmail.com>
2025-02-27 02:22:20 -0800

    Add again and exit button

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Install ``pip install pytubefix``
 
 This is a program in download youtube videos. Currently with how YouTube efficiently streams videos using DASH, this program downloads the highest video stream and audio stream available and merges the files into a single mp4 file. The resulting file will be stored on the desktop.
 
+Cannot naturally read codec av01 on windows. Suggested to use VLC media player
+
 Currently uses pytubefix with ffmpeg to merge the streams.
--- mainwindow.py
+++ mainwindow.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QLineEdit, QPushButton, QProgressBar
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt
 from downloader import Downloader
@@ -12,6 +12,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("YT Downloader")
         self.setGeometry(700, 300, 600, 500)
         self.setWindowIcon(QIcon("icon.jpg"))
+        self.setStyleSheet("background-color: #636363;")       
         
         # Top Banner Label
         label = QLabel("Welcome to YT Downloader!", self)
@@ -32,14 +33,27 @@ class MainWindow(QMainWindow):
         self.line_edit.setPlaceholderText("Enter link of video")
 
         # Button component
-        self.button = QPushButton("Download", self)
-        self.button.setGeometry(200, 150, 200, 50)
-        self.button.setStyleSheet("font-size: 20px;"
+        self.download_button = QPushButton("Download", self)
+        self.download_button.setGeometry(50, 150, 200, 50)
+        self.download_button.setStyleSheet("font-size: 20px;"
                                   "font-family: Arial")
-        self.button.clicked.connect(self.submit)
+        self.download_button.clicked.connect(self.submit)
+
+        # Again Button
+        self.again_button = QPushButton("Again", self)
+        self.again_button.hide()
+        self.again_button.setGeometry(50, 350, 200, 50)
+        self.again_button.clicked.connect(self.restart)
+
+        # Exit Button
+        self.exit_button = QPushButton("Exit", self)
+        self.exit_button.hide()
+        self.exit_button.setGeometry(300, 350, 200, 50)
+        self.exit_button.clicked.connect(lambda: {self.close()})
 
         # Progress Bar
         self.pbar = QProgressBar(self)
+        self.pbar.hide()
         self.pbar.setGeometry(50, 250, 500, 50)
         
         # Window Downloader
@@ -49,10 +63,31 @@ class MainWindow(QMainWindow):
     def submit(self):
         text = self.line_edit.text()
         print(f"Entered: {text}")
+        self.downloading_state()
         self.downloader.run(text)
+        self.finished_state()
 
     def update_progress(self, value):
         self.pbar.setValue(value)
+
+    def downloading_state(self):
+        self.pbar.show()
+        self.line_edit.hide()
+        self.download_button.hide()
+
+    def finished_state(self):
+        self.again_button.show()
+        self.exit_button.show()
+
+    def restart(self):
+        self.pbar.hide()
+        self.line_edit.clear()
+        self.line_edit.show()
+        self.download_button.show()
+        self.again_button.hide()
+        self.exit_button.hide()
+        self.pbar.setValue(0)
+
 
 def launch():
     app = QApplication([])
